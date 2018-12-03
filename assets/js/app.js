08f6b86a7129b4b6ca0f969bcd9d9f9520bf8800
MCorey <mjcorey57@gmail.com>
2018-12-02 21:57:03 -0700

    Testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,16 @@
-var constraints = {video: { facingMode:"environment" }, audio: false};
+var constraints = {video: { facingMode:"user" }, audio: false};
 
 const cameraView = document.querySelector("#camera--view"),
-cameraOutput = document.querySelector("#camera--output"),
-cameraSensor = document.querySelector("#camera--sensor"),
-cameraTrigger = document.querySelector("#camera--trigger")
+      cameraOutput = document.querySelector("#camera--output"),
+      cameraSensor = document.querySelector("#camera--sensor"),
+      cameraTrigger = document.querySelector("#camera--trigger")
 
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
         track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
+        cameraView.srcObject = stream
     })
     .catch(function(error) {
         console.error("Oops. Something is broken.", error);
